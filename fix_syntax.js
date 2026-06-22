7e1b0558f077b0de041e5cee3ebdfb9e21628674
unknown <maged.ghabour2016@gmail.com>
2026-06-22 10:55:59 +0300

    add Whatsapp

---
+++ fix_syntax.js
@@ -0,0 +1,4 @@
+﻿const fs = require('fs');
+let t = fs.readFileSync('functions.php', 'utf8');
+t = t.replace("\t'location' => array(", "\t),\n\t'location' => array(");
+fs.writeFileSync('functions.php', t, 'utf8');
---
+++ update_cpt.js
@@ -0,0 +1,16 @@
+﻿const fs = require('fs');
+let lines = fs.readFileSync('functions.php', 'utf8').split('\n');
+let newLines = [];
+let skip = false;
+for (let i = 0; i < lines.length; i++) {
+    if (lines[i].includes('// BEFORE/AFTER SECTION')) {
+        skip = true;
+    }
+    if (skip && lines[i].includes("'location' => array(")) {
+        skip = false;
+    }
+    if (!skip) {
+        newLines.push(lines[i]);
+    }
+}
+fs.writeFileSync('functions.php', newLines.join('\n'), 'utf8');
